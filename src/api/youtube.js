e04ae3381763239e8c3ab859bb0094df1adae21e
Hermetheus <allanmolson@gmail.com>
2019-08-04 20:32:27 -0500

    youtube Api Issue

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://www.googleapis.com/youtube/v3'
-})
-=======
-  baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    maxResults: 5,
-    key: ''
-  }
-})
->>>>>>> c89b5e92858b9c865a863af0d9171cf2f1e940b5
+  baseURL: "https://www.googleapis.com/youtube/v3"
+});
